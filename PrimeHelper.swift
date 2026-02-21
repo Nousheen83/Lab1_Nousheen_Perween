407ecd6dbc36fbd04b1e658b7430d45143e933c4
Nousheen82 <10september83@gmail.com>
2026-02-21 15:27:15 +0500

    Optimized prime checking logic

--- PrimeHelper.swift
+++ PrimeHelper.swift
@@ -4,9 +4,9 @@ struct PrimeHelper {
     
     static func isPrime(_ num: Int) -> Bool {
         if num <= 1 { return false }
-        if num == 2 { return true }
+        if num <= 3 { return true }
         
-        for i in 2..<num {
+        for i in 2...Int(sqrt(Double(num))) {
             if num % i == 0 {
                 return false
             }
