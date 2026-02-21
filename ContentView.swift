d27a348c7e982e7ca63632145c337aa723dc8451
Nousheen82 <10september83@gmail.com>
2026-02-21 15:13:18 +0500

    Connected buttons with prime logic and score tracking

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var number = Int.random(in: 1...100)
+    @State private var correctCount = 0
+    @State private var wrongCount = 0
     
     var body: some View {
         VStack(spacing: 30) {
@@ -16,14 +18,26 @@ struct ContentView: View {
             
             HStack(spacing: 40) {
                 Button("Prime") {
-                    
+                    checkAnswer(userAnswer: true)
                 }
                 
                 Button("Not Prime") {
-                    
+                    checkAnswer(userAnswer: false)
                 }
             }
         }
         .padding()
     }
+
+    func checkAnswer(userAnswer: Bool) {
+    let correctAnswer = PrimeHelper.isPrime(number)
+    
+    if userAnswer == correctAnswer {
+        correctCount += 1
+    } else {
+        wrongCount += 1
+    }
+    
+    number = Int.random(in: 1...100)
+}
 }
